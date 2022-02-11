b1e4c4d28bb0b0f7b9df4d6a7f0d52dad88b2f83
NI210248 <NI210248@reigate.ac.uk>
2022-02-11 10:35:19 +0000

    added frame

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import javax.swing.*;
+import javax.swing.JFrame;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        JFrame frame = new JFrame(); // creates a frame
+        frame.setTitle("pong"); // title of frame
+        frame.setResizable(false);
+        frame.setSize(420,420); // sets x and y dimension of frame
+        frame.setVisible(true); // make frame visible
+
+        ImageIcon icon = new ImageIcon("com/company/pong_icon.png");
+        frame.setIconImage(icon.getImage());
     }
 }
