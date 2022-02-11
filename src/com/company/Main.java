d8a26794f7fd193cb4c8bf815dffc5a89e2309a2
Nick Izbrodin <nizbrodin@gmail.com>
2022-02-11 14:49:50 +0000

    added icon

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,10 +10,10 @@ public class Main {
         JFrame frame = new JFrame(); // creates a frame
         frame.setTitle("pong"); // title of frame
         frame.setResizable(false);
-        frame.setSize(420,420); // sets x and y dimension of frame
+        frame.setSize(960,540); // sets x and y dimension of frame
         frame.setVisible(true); // make frame visible
 
         ImageIcon icon = new ImageIcon("pong_icon.png"); // create an ImageIcon
-        frame.setIconImage(icon.getImage()); // change icon of frame
+        frame.setIconImage(icon.getImage()); // change icon of frame to pong_icon.png
     }
 }
