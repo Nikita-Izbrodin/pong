9265b2a4fd25ca6d024da59fe88a2fad91cfab39
Nick Izbrodin <90528953+Nick-Izbrodin@users.noreply.github.com>
2022-02-11 15:27:26 +0000

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,10 @@ public class Main {
 
     static void menu() {
         JFrame frame = new JFrame(); // creates a frame
-        frame.setTitle("pong"); // title of frame
+        frame.setTitle("pong");
         frame.setResizable(false);
-        frame.setSize(960,540); // sets x and y dimension of frame
-        frame.setVisible(true); // make frame visible
+        frame.setSize(960,540);
+        frame.setVisible(true);
 
         ImageIcon icon = new ImageIcon("pong_icon.png"); // create an ImageIcon
         frame.setIconImage(icon.getImage()); // change icon of frame to pong_icon.png
