7e333882d259a89b196f60411a674a8ef931e01e
JS211384 <JS211384@reigate.ac.uk>
2022-03-14 14:02:19 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/gameFolder/Menu.java

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -37,8 +37,6 @@ public class TileManager {
         else if (Menu.globalDifficulty ==2){
             loadMap("/resources/maps/map01.txt");
         }
-        // add serenade mode maps
-
     }
 
 
